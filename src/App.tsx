cf8c73cc15ebc6fe5b7e82b0c79fbba6271f1c3e
Juho-Veikko Hytönen <jv.hytonen@gmail.com>
2022-04-14 11:10:03 +0300

    First tests done and fetch-method working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 
-
+type doSomethingType = () => void
 function App() {
+  console.log(process.env.TEST)
+
+const doSomething: doSomethingType = async () => {
+ const response = await fetch('https://4r294n67b7.execute-api.eu-north-1.amazonaws.com/dev/flight-data')
+ const data = await response.json()
+ console.log(data)
+
+}
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
+    <div className="App h-full">
+      <header className="App-header h-full">
+        <div className='h-full'>
+          <h1 onClick={doSomething}>Hello</h1>
+        </div>
       </header>
     </div>
   );
