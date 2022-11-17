2ec70817a1e308b100ca2643ea9ddc6da6610849
Juho-Veikko Hytönen <jv.hytonen@gmail.com>
2022-11-17 14:27:38 +0200

    New repo for backend code created. Unnecessary files deleted, only Lambda left.

--- src/Utils/AirportIataToCity.js
+++
@@ -1,5 +0,0 @@
-import { airports } from "./airports"
-
-export const IataToCity = ( iataCode ) => {
-    return airports[iataCode]
-}
--- src/Utils/FormatDate.tsx
+++
@@ -1,4 +0,0 @@
-
-export const formatDate = (date: string) => {
-    return date.slice(-5)
-}
